52eed1eb99f91a05b91bdcc2448e84190d44323b
Jackerman09 <jackerman09@gmail.com>
2014-10-04 14:55:13 -0400

    generate devise:install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Rails.application.configure do
 
   # Ember
   config.ember.variant = :development
+
+  # Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
