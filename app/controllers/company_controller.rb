9b59a1fe50b0a0d287f915f96325ae598589067e
Jackerman09 <jackerman09@gmail.com>
2014-10-23 19:29:36 -0400

    add company controller

---
+++ app/controllers/company_controller.rb
@@ -0,0 +1,17 @@
+class CompanyController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :company, only: [:index, :show, :create, :update, :destroy]
+
   root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/ember'
